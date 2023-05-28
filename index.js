fd81fcdf1290ee879cb5b12b64fb1c3eeeacdd40
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-28 19:15:07 +0300

    feat: add burger menu functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+const headerNav = document.querySelector(".header__nav");
+
+const handleMenuClick = (e) => {
+  const target = e.target;
+  if (
+    target.classList.contains("hamburger") ||
+    target.classList.contains("hamburger__line")
+  ) {
+    document.documentElement.classList.toggle("lock");
+    headerNav.classList.toggle("nav_active");
+  }
+  if (
+    target.classList.contains("header__nav") ||
+    target.classList.contains("nav__link")
+  ) {
+    document.documentElement.classList.toggle("remove");
+    headerNav.classList.remove("nav_active");
+  }
+};
+
+header.addEventListener("click", handleMenuClick);
